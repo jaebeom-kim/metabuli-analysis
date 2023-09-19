301c6d9f41ef0a4a975581ab0d294b693e190d09
Jaebeom Kim <jbeom0731@gmail.com>
2023-09-19 10:18:47 +0900

    add scripts for database creation

---
+++ ProkaryoteBenchmark/database_creation/exclusion/krakenuniq/krakenuniq_ex_build.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# Reference: https://github.com/fbreitwieser/krakenuniq
+
+# Taxonomy files
+cp -r ../../taxonomy /mnt/scratch/jaebeom/gtdb_202_exclusion/db/krakenuniq
+
+# copy seqid2taxid.map (sequence id to taxid mapping) to directory containing fasta files for DB build
+cp seqid2taxid.map /mnt/scratch/jaebeom/gtdb_202_exclusion/db_genomes
+
+~/krakenuniq-1.0.3/krakenuniq-build --db /mnt/scratch/jaebeom/gtdb_202_exclusion/db/krakenuniq \
+    --library-dir /mnt/scratch/jaebeom/gtdb_202_exclusion/db_genomes \
+    --threads 32
