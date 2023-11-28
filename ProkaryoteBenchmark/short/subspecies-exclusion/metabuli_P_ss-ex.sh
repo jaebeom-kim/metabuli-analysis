a5b9344d4f82b5d4a315f936c01bd540ff6ec7a4
Jaebeom Kim <jbeom0731@gmail.com>
2023-11-28 16:10:16 +0900

    update README

--- ProkaryoteBenchmark/short/subspecies-exclusion/metabuli_P_ss-ex.sh
+++ ProkaryoteBenchmark/short/subspecies-exclusion/metabuli_P_ss-ex.sh
@@ -9,10 +9,3 @@
 	ss-excluison_short_P_1110 \
 	> /fast/jaebeom/gtdb-ss-exclusion/results/ss-excluison_short_P_1110.log
 	
-#  ~/dev/Metabuli/build-release/src/metabuli classify --threads 32 --min-sp-score 0.5 --min-score 0.15 \
-#  	/mnt/scratch/jaebeom/gtdb_202_ss_exclusion/query_reads/prokaryote_ss_exclusion_reads_1.fna \
-# 	/mnt/scratch/jaebeom/gtdb_202_ss_exclusion/query_reads/prokaryote_ss_exclusion_reads_2.fna \
-# 	/mnt/scratch/jaebeom/gtdb_202_ss_exclusion/db/metabuli \
-# 	/mnt/scratch/jaebeom/gtdb_202_ss_exclusion/results/metabuli \
-# 	ss-excluison_short_P \
-# 	> /mnt/scratch/jaebeom/gtdb_202_ss_exclusion/results/metabuli/ss-excluison_short_P.log
